e20f64dcadd80e7f312de1634cb1e4ad4572a492
alfakr <dani.alfakr@gmail.com>
2014-09-22 14:16:40 +0700

    Update tag.py

--- tag.py
+++ tag.py
@@ -5,7 +5,7 @@ mode = "\x00"
 add_blk = 00
 num_blk = 01
 snr = create_string_buffer("\xFF\xFF\xFF\xFF\xFF\xFF")
-print repr(snr.raw)
+#print repr(snr.raw)
 buffer = (c_ubyte*16)()
 
 tag = nativeLib.API_PCDRead(mode, add_blk, num_blk, byref(snr), byref(buffer))
